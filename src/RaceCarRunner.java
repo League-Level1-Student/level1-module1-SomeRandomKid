9520ab21cee47ec6dae67db35e57098a932b3e42
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-26 17:05:43 -0700

    hi

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,18 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar boi = new RaceCar("Why", 5);
 		// 3. Get the RaceCar to print its position in the race.
-
+		System.out.println(boi.getPositionInRace());
 		// 4. Crash the RaceCar
-		
+		boi.crash();
 		// 5. If the car is damaged. Bring it in for a pit stop.
-
+		if(boi.damaged == true) {
+			boi.pit();
+		}
 		// 6. Help the car move into first place.
-
+		for (int i = 0; i < 7; i++) {
+		boi.overtake();
+		}
 	}
 }
